422276cd75e91cde3392e7e3ba7d4c2c35ae8e88
codewithprags <pragati.dwivedi@outlook.com>
2025-04-09 16:34:22 -0400

    data wrangling

--- code/data_processing.R
+++ code/data_processing.R
@@ -1,24 +1,49 @@
 library(tidyverse)
 library(ggplot2)
-
+library(DESeq2)
 library(GEOquery)
 library(limma)
 library(umap)
 
-# load series and platform data from GEO
+# load series and platform data from GEO#####
 
 gset <- getGEO("GSE231994", GSEMatrix =TRUE, getGPL=FALSE)
 if (length(gset) > 1) idx <- grep("GPL27956", attr(gset, "names")) else idx <- 1
 gset <- gset[[idx]]
 
-# ex <- exprs(gset)
-# # log2 transform
+
+summary(gset)
+dim(gset)
+dim(df)
+
+expr_data <- exprs(gset)
+pheno_data <- gset@phenoData@data
+expr_df <- as.data.frame(expr_data)
+
+head(expr_df)
+head(pheno_data)
+
+expr_data_log <- log2(expr_data +1)
+
+diagnosis <- pheno_data$characteristics_ch1.1
+diagnosis <- gsub("treatment: ","", diagnosis)
+
+table(diagnosis)
+
+#log2 transform
 # qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
 # LogC <- (qx[5] > 100) ||
-#   (qx[6]-qx[1] > 50 && qx[2] > 0)
+# (qx[6]-qx[1] > 50 && qx[2] > 0)
 # if (LogC) { ex[which(ex <= 0)] <- NaN
 # ex <- log2(ex) }
-# 
+
+
+
+
+
+
+
+
 # # box-and-whisker plot
 # dev.new(width=3+ncol(gset)/6, height=5)
 # par(mar=c(7,4,2,1))
@@ -45,7 +70,7 @@ gset <- gset[[idx]]
 
 
 
-###PROCESSED DATA###
+###PROCESSED DATA####
 df <- read.table('GSE232050_processed_data.txt', 
                  sep = '\t',
                  header = TRUE)
