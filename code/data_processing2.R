86e8911f45ada8dc93a3cf79574256725c2e8b75
codewithprags <pragati.dwivedi@outlook.com>
2025-04-11 13:51:26 -0400

    yess

--- code/data_processing2.R
+++ code/data_processing2.R
@@ -60,7 +60,37 @@ ggplot(results, aes(logFC, -log10(adj.P.Val))) +
   geom_vline(xintercept=c(-1,1), linetype="dashed")
 
 
+####Enhanced Volcano Plot#####
 
+
+res <- read.csv("DE_results_PD_vs_psPD.csv") 
+rownames(res) <- res$X  # Uses the gene names column
+res$X <- NULL 
+
+print(colnames(res))  # Should show: logFC, AveExpr, t, P.Value, adj.P.Val, B
+
+# Proper LIMMA-compatible EnhancedVolcano plot
+EnhancedVolcano(res,
+                lab = rownames(res),  # Probe IDs as labels
+                x = 'logFC',          
+                y = 'P.Value', 
+                ylim = c(0, 10),  # Sets y-axis limit to -log10(p) = 10
+                xlab = bquote(~Log[2]~ 'fold change'),
+                ylab = bquote(~-Log[10]~italic(P)),
+                title = 'PD vs psPD Differential Expression',
+                subtitle = "LIMMA results",
+                FCcutoff = 1,        # Fold change threshold (|log2FC| > 1)
+                pCutoff = 0.05,      # Significance threshold
+                pointSize = 1,
+                labSize = 5,
+                col = c("grey30", "forestgreen", "royalblue", "red2"),
+                colAlpha = 0.7,
+                legendPosition = 'right',
+                drawConnectors = TRUE,
+                widthConnectors = 0.1,
+                max.overlaps = 20) +  # Limits label overlaps
+  scale_x_continuous(trans = 'reverse') +  # Properly reverses x-axis
+  theme_minimal(base_size = 14)
 
 
 
